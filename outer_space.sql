d5a125d583a62569ea559bf53d3b0ea6fbb15a41
Lexsa Campbell <lexsacampbell@Lexsas-MacBook-Pro.local>
2023-05-14 00:27:21 -0400

    Split planets data into orbits, planets_moons, moons, and galaxies tables"

--- outer_space.sql
+++ outer_space.sql
@@ -12,17 +12,42 @@ CREATE TABLE planets
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy_id INTEGER NOT NULL references galaxies(id)
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+CREATE TABLE orbits 
+(
+  id SERIAL PRIMARY KEY,
+  main_planet INTEGER NOT NULL references planets(id), 
+  orbit_planet INTEGER NOT NULL references planets(id) 
+);
+
+CREATE TABLE planets_moons
+(
+  id SERIAL PRIMARY KEY,
+  planet_id INTEGER NOT NULL references planets(id),
+  moon_id INTEGER NOT NULL references moons(id)
+);
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
